ce1491f367b5b3160b2cfe2692d6783b2521ce68
Stelf <larodi@gmail.com>
2014-10-24 15:19:24 +0300

    fix tabs and add url

--- up-23.10/float.anomaly.cpp
+++ up-23.10/float.anomaly.cpp
@@ -4,19 +4,19 @@
 #include "stdafx.h"
 
 int main() {
-	int a = 0; 
-	a = 530;
+    int a = 0; 
+    a = 530;
 
-	float b = a * 1.255;
+    float b = a * 1.255;
 
-	// правилния начин за сравняване на floating point числа
-	if (b - 665.15 < 0.01) { 
-		printf ("yeah!");
-	}
-	int c = b;
+    // правилния начин за сравняване на floating point числа
+    if (b - 665.15 < 0.01) { 
+    	printf ("yeah!");
+    }
+    int c = b;
 
-	printf("%u", c);
-	return 0;
+    printf("%u", c);
+    return 0;
 }
 
 
--- up-23.10/gcd.cpp
+++ up-23.10/gcd.cpp
@@ -3,28 +3,29 @@
 
 #include "stdafx.h"
 
+// http://upload.wikimedia.org/wikipedia/commons/3/37/Euclid%27s_algorithm_Book_VII_Proposition_2_3.png
 
 int main() {
-	int a, b;
+    int a, b;
 
-	scanf_s("%d %d", &a, &b);
+    scanf_s("%d %d", &a, &b);
 
-	if (a == 0) {
-		printf("%d", b);
-	} else {
-		while (b != 0 && b != a) {
-			printf("a: %d b: %d \n", a, b);
+    if (a == 0) {
+    	printf("%d", b);
+    } else {
+    	while (b != 0 && b != a) {
+    		printf("a: %d b: %d \n", a, b);
 
-			while (a > b) {
-				a = a - b;
-			} 
+    		while (a > b) {
+    			a = a - b;
+    		} 
 
-			while (b > a) {
-				b = b - a;
-			}
-		}
-		printf("%d", a);
-	}
+    		while (b > a) {
+    			b = b - a;
+    		}
+    	}
+    	printf("%d", a);
+    }
 }
 
 
