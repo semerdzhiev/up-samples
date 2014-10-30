1a0649058defcff785a80376df468be4ef07dee8
Stelf <larodi@gmail.com>
2014-10-30 15:04:26 +0200

    fixes to GCD example

--- up-23.10/gcd.cpp
+++ up-23.10/gcd.cpp
@@ -3,6 +3,11 @@
 
 #include "stdafx.h"
 
+// gcd.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
 // http://upload.wikimedia.org/wikipedia/commons/3/37/Euclid%27s_algorithm_Book_VII_Proposition_2_3.png
 
 int main() {
@@ -12,20 +17,37 @@ int main() {
 
     if (a == 0) {
     	printf("%d", b);
-    } else {
-    	while (b != 0 && b != a) {
-    		printf("a: %d b: %d \n", a, b);
+		return 0;
+    } 
 
-    		while (a > b) {
-    			a = a - b;
-    		} 
-
-    		while (b > a) {
-    			b = b - a;
-    		}
-    	}
+	if (b == 0) {
     	printf("%d", a);
-    }
-}
+		return 0;
+    } 
 
+	// : disambiguation : 
+	// || - logical or
+	// && - logical and
+	// | - bitwise or
+	// & - bitwise and
+
+    while (b != 0 && b != a) {
+    	printf("a: %d b: %d \n", a, b);
+
+//		we can express the loop as modulo 
+//		if ( a > b ) a = a % b; 
+    	while (a > b) {
+    		a = a - b;
+    	}
+
+//		we can express the loop as modulo 
+//		else b = b % a; 
+    	while (b > a) {
+    		b = b - a;
+    	}
+    }
+
+   	printf("GCD : %d", a);
+	return 0;
+}
 
