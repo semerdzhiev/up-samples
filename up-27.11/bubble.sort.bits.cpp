797aa7278c2f83dd223e9a92efad772229fa33c2
Stelf <larodi@gmail.com>
2014-11-27 15:02:40 +0200

    sorting numbers based on the count of their bits

---
+++ up-27.11/bubble.sort.bits.cpp
@@ -0,0 +1,77 @@
+// sort.bits.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+#include <iostream>
+
+unsigned char nums[] =  {64, 55, 46, 24, 13, 114, 211, 212};
+
+int numbits(unsigned char a) {
+	int res = 0;
+
+	do { 
+		if ( a & 1 ) {
+			res ++;
+		}
+		a = a >> 1; // a /= 2
+	} while( a );
+
+	return res;
+}
+
+void asbin(unsigned char a) { 
+	for (int i = 7; i >= 0; i--)
+	{
+		int mask = 1 << i;
+		if (a & mask)
+		{
+			std::cout << 1;
+		}
+		else { 
+			std::cout << 0;
+		}
+	}
+}
+
+void printall() {
+	for (int i = 0; i < sizeof(nums) / sizeof(nums[0]); i++ ) {
+		asbin(nums[i]);
+		std::cout << '(' << numbits(nums[i]) << ')' << std::endl;
+	}
+
+	std::cout << std::endl;
+}
+
+void swap(int i, int j) {
+	int t = nums[j];
+	nums [j] = nums [i];
+	nums [i] = t;
+
+}
+
+int main() {
+	printall();
+
+	bool change;
+
+	do { 
+		change = false;
+
+		for (int i = 0; i < sizeof(nums) / sizeof(nums[0]) - 1; i++ )
+		{
+			if (numbits(nums[i]) > numbits(nums [i+1]))
+			{
+				swap(i, i+ 1);
+				change = true;
+			}
+		}
+
+	} while (change);
+
+	printall();
+	return 0;
+}
+
+
