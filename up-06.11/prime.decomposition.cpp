f81a71637feba1abebc41224fdd7fd70add4a1b5
Stelf <larodi@gmail.com>
2014-12-11 14:29:23 +0200

    add some comments to existing examples.add bitwise.cpp

--- up-06.11/prime.decomposition.cpp
+++ up-06.11/prime.decomposition.cpp
@@ -1,5 +1,8 @@
 //
 
+/* print the decomposition of a number to it's prime factors */
+
+
 #include "stdafx.h"
 #include <stdio.h>
 
@@ -7,12 +10,16 @@ using namespace std;
 
 int primes[100];
 
-// 0, 1, 2, 3, .... 9 
-
 int main() {
 	int counter = 0;
 	unsigned long mynum = 2*2*3*3*5;
 	int i = 2;
+
+	// while modulo of the number by some divisor 
+	// gives zero -> note this divisor of prime factor
+	// of the number
+	// divide the number and continue as long as
+	// the current divisor is less than the number
 
 	do {
 		while(mynum % i == 0) {
