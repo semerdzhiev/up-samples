4cd33627ec68c8efaf0bd3690c3bb880b5953c60
Stelf <larodi@gmail.com>
2014-12-11 15:01:00 +0200

    some more comments and generalisation

--- up-11.12/longest.greater.seq.cpp
+++ up-11.12/longest.greater.seq.cpp
@@ -1,23 +1,14 @@
 // ConsoleApplication1.cpp : Defines the entry point for the console application.
 //
 
-/* 
-
-task: among numbers fread from standard input, 
-find the size of the longest sequence of numbers greater than 128 
-
-i.e. in 
-
-3 56  21 34 75 3 424 111 9 128 128 128 23 7457 456 342 5435 4 4 
-
-the longest sequence has length 4
-
-*/
-
 #include "stdafx.h"
 
 #include <iostream>;
 using namespace std;
+
+bool cond(int num) {
+	return num > 128;
+}
 
 int main()  {
 	int nmax = 0;
@@ -27,10 +18,11 @@ int main()  {
 	do { 
 		cin >> num;
 		
-		if (num > 128) { 
+		// if current number meets condition
+		if (cond(num)) { 
 			cmax ++;
 		} else { 
-			if ( cmax > nmax ) {
+			if (cmax > nmax) {
 				nmax = cmax;
 				cmax = 0;
 			}
